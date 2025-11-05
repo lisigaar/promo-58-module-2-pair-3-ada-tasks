1651e681302dc59b9e3c65381846785d462b678c
Jennifer <jenniferromanmuerte@gmail.com>
2025-11-05 10:26:21 +0100

    adding gitHubUser

--- main.js
+++ main.js
@@ -2,14 +2,18 @@
 
 const listElement = document.querySelector(".js_list_task");
 const task = document.querySelectorAll(".task");
-const SERVER_URL = `https://dev.adalab.es/api/todo`;
+const GITHUB_USER = "jenniferromanmuerte";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
 let tasks = [];
 
 function renderTask(tasks) {
   for (const task of tasks) {
     if (task.completed === true) {
-      listElement.innerHTML += `<li class="task completed">${task.name}</li>`;
+      listElement.innerHTML += `
+      <li class="task completed">${task.name}
+      
+      </li>`;
     } else {
       listElement.innerHTML += `<li class="task">${task.name}</li>`;
     }
